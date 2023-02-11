f6d1df88e9a76ae7e1bb4e369942e312f60534d5
rogerhself@gmail.com <rogerhself@gmail.com>
2023-02-11 13:25:21 -0600

    Update

--- Day1.py
+++ Day1.py
@@ -1,1 +1,20 @@
-print("Hello World")
+#Strings
+
+# print("Hello World \nHello World")
+
+# print("Hello " + "Roger")
+
+
+
+
+#Input Functions
+
+# name = input("What is your name?")
+# print("Hello " + name +", nice to meet you!")   # using concatenation with variable name
+# print(f"Hello {name}, nice to meet you!")       # using f-string to insert input
+
+
+#Variables
+
+name = input("What is your name?")  # Assigns input to variable called "name"
+
