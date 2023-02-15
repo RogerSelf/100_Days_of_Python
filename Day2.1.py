0f71a6344d9854d2e30ad34653dad7164910fcb7
rogerhself@gmail.com <rogerhself@gmail.com>
2023-02-14 22:05:51 -0600

    Update

---
+++ Day2.1.py
@@ -0,0 +1,12 @@
+#Type Errors, Type Checking, Typer Conversion
+
+
+
+num_char = len(input("What is your name? "))
+
+print("Your name has " + str(num_char) + " characters")
+
+#print(type(num_char))  - checking type of a var
+
+
+
