70b9d01749ed09ac3a5b5957d7f48e66ec76863b
rogerhself@gmail.com <rogerhself@gmail.com>
2023-02-14 21:56:51 -0600

    Update

--- Day2.py
+++ Day2.py
@@ -1,3 +1,21 @@
 # _Primitive Data Types_
 
+#Subscripting
+print("Hello"[0])  #prints H as it's the first letter in the word (starting at 0)
+
+#String
+"Hello"
+
+#Integer
+134
+4344352
+
+#Float  (non integer numbers)
+float = 3.14
+
+#Boolean
+True
+False
+
+
 
